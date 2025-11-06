283a589495ac92375e00f836ebf0ce8aaf08d3ab
Åström Hugo E A <hugo.astrom@helsinki.fi>
2025-11-06 09:52:07 +0200

    Cleanup

--- project/qiskit-iqm/venv/noisy-tests/adapt_vqe.py
+++ project/qiskit-iqm/venv/noisy-tests/adapt_vqe.py
@@ -14,109 +14,272 @@ from scipy.optimize import minimize
 
 class QubitAdaptVQE():
 
-    def __init__(self, ansatz, hamiltonian, nqubits, optimizer):
+    def __init__(self, ecore, h1e, h2e, optimizer):
        """
        Args:
-           ansatz (QuantumCircuit): 
-           hamiltonian (SparsePauliOp): 
-           n_qubits (int): 
-           optimizer (string): 
+           ecore (float): Core Hamiltonian
+           h1e (np.ndarray): 1-electorn integrals
+           h2e (np.ndarray): 2-electron integrals
+           optimizer (string): Classical SciPy optimizer algorithm
        """
 
-       self.ansatz = ansatz
-       self.hamiltonian = hamiltonian
-       self.nqubits = nqubits
+       self.ecore = ecore
+       self.h1e = h1e
+       self.h2e = h2e
        self.optimizer = optimizer
+       self.backend = None
 
-    def run_qc(self, qc, shots):
-        backend = IQMFakeAdonis()
+       self.hamiltonian = self.build_hamiltonian()
+       self.nqubits = self.hamiltonian.num_qubits
+       self.operator_pool = self.generate_pool()
+       self.appended_ops = [SparsePauliOp(["I" * self.nqubits])]
+       self.paramstring = [0.0]
+       
+    def set_backend(self, backend):
+        self.backend = backend
+
+    def run_qc(self, qc: QuantumCircuit, shots: int):
+        if backend == None:
+            raise ValueError("Backend not set!")
+        
+        backend = self.backend
         trans_c = transpile(qc, backend=backend)
         job = backend.run(trans_c, shots=shots)
         result = job.result()
         exp_result = job.result()._get_experiment(qc)
 
-        print(result.get_counts())
+        return result
 
-    def commutator(self, a, b, appended_ops, paramstring):
+    def commutator(self, a: SparsePauliOp, b: SparsePauliOp):
+        """
+        Measure commutator AB - BA to obtain energy derivatives dE / d\theta_i = <psi|[H, O_i]|psi>
+        args:
+            a (SparsePauliOp): First operator
+            b (SparsePauliOp): Second operator
+        """
 
-        psi = QuantumCircuit(nqubits)
-        paulistring = sum(appended_ops)
+        # Prepare quantum state
+        psi = self.state_prep()
+        paulistring = sum(self.appended_ops)
         evolution = PauliEvolutionGate(paulistring, time=1)
         psi.compose(evolution, inplace=True)
 
         operator = a @ b - b @ a
 
+        # Measure value
         estimator = Estimator()
         exp_val = estimator.run(psi, operator).result().values
 
+        # The derivative of the energy with resoect to parameter i gets a complex phase
         exp_val = 1.0j * exp_val
-        print("\nOperator:", b, "Commutator:", exp_val, "\n")
 
         return exp_val
 
 
-    def energy(self, paramstring):
+    def energy(self, params):
+        """
+        Measure energy
+        """
+        psi = self.state_prep()
 
-        psi = QuantumCircuit(nqubits)
-
-        paulistring = SparsePauliOp([op.paulis[0] for op in appended_ops], coeffs = paramstring)
+        paulistring = SparsePauliOp([op.paulis[0] for op in self.appended_ops], coeffs = params)
         evolution = PauliEvolutionGate(paulistring, time=1)
         psi.compose(evolution, inplace=True)
 
         estimator = Estimator()
-        e = estimator.run(psi, q_hamiltonian).result().values
+        e = estimator.run(psi, self.hamiltonian).result().values[0]
 
         return e
 
-    def optimize_params(self, paramstring):
+    def optimize_params(self, args={}):
+        """
+        Classically optimize parameter vector
+        args:
+            self.paramstring (list): Parameter vector to optimize
+            args (dict): Optional arguments for the SciPy optimizer
+        returns:
+            Optimized parameter vector
+        """
 
-        theta = paramstring
-        res = minimize(energy, theta, method = self.optimizer, options={'xatol': 1e-8, 'disp': True})
+        res = minimize(energy, self.paramstring, method = self.optimizer, options=args)
 
         return res.x
 
-    def minimize_energy(self, nshots, maxiter):
+    def generate_pool(self):
+        """
+        Generate operator pool
+        """
 
-        operator_pool = [SparsePauliOp(["ZY"]), SparsePauliOp(["YI"])]
-        #qubit_hamiltonian = mapped(fermion_hamiltonian) # From LUCAS/PySCF?
-        q_hamiltonian = SparsePauliOp(["IZ", "ZI"], coeffs = [1 / 2, 1 / 2])
-        #ansatz = mapped(HF_state) # From LUCAS/PySCF?
+        op_strings = []
+        for p in range(self.nqubits - 1):
+            q = self.nqubits - p - 2
+            op_strings.append("I" * p + "ZY" + "I" * q)
+        for p in range(self.nqubits - 1):
+            q = self.nqubits - p - 1
+            op_strings.append("I" * p + "Y" + "I" * q)
+                
+        pool = [SparsePauliOp(op) for op in op_strings]
+                
+        return pool
 
-        nqubits = 2
-        shots = 1000
-        max_iter = 50
+    def state_prep(self):
+        """
+        Initialize quantum circuit
+        """
+        
+        psi = QuantumCircuit(self.nqubits)
 
+        return psi
+
+    def cholesky(self, eps):
+        """
+        Decompose two-body term in Hamiltonian to lower rank
+        args:
+            V (numpy ndarray): Two-electron integrals
+            eps (float): Error resulting from decomposition
+        returns:
+            Numpy array representing the low-rank decomposition of the Hamiltonian two-body terms            
+        """
+
+        # see https://arxiv.org/pdf/1711.02242.pdf section B2
+        # see https://arxiv.org/abs/1808.02625
+        # see https://arxiv.org/abs/2104.08957
+        no = self.h2e.shape[0]
+        chmax, ng = 20 * no, 0
+        W = self.h2e.reshape(no**2, no**2)
+        L = np.zeros((no**2, chmax))
+        Dmax = np.diagonal(W).copy()
+        nu_max = np.argmax(Dmax)
+        vmax = Dmax[nu_max]
+        while vmax > eps:
+            L[:, ng] = W[:, nu_max]
+            if ng > 0:
+                L[:, ng] -= np.dot(L[:, 0:ng], (L.T)[0:ng, nu_max])
+            L[:, ng] /= np.sqrt(vmax)
+            Dmax[: no**2] -= L[: no**2, ng] ** 2
+            ng += 1
+            nu_max = np.argmax(Dmax)
+            vmax = Dmax[nu_max]
+        L = L[:, :ng].reshape((no, no, ng))
+        print(
+            "accuracy of Cholesky decomposition ",
+            np.abs(np.einsum("prg,qsg->prqs", L, L) - self.h2e).max(),
+        )
+        return L, ng
+
+    def identity(self, n):
+        """
+        n-dimensional identity operator on the space of n qubits
+        args:
+            n (int): Number of qubits
+        returns:
+            SparsePauliOperator representing the identity operator
+        """
+
+        return SparsePauliOp.from_list([("I" * n, 1)])
+
+    def qubit_mapping(self, n, mapping = "jordan_wigner"):
+        """
+        Map bosonic operators to fermionic operators
+        args:
+            n (int): Number of qubits
+            mapping (str): Map bosons -> fermions
+        returs:
+            Lists of creation and annihilation operators
+        """
+
+        c_list = []
+        if mapping == "jordan_wigner":
+            for p in range(n):
+                ell, r = "I" * (n - p - 1), "Z" * p
+                cp = SparsePauliOp.from_list([(ell + "X" + r, 0.5), (ell + "Y" + r, 0.5j)])
+                c_list.append(cp)
+        else:
+            raise ValueError("Unsupported mapping.")
+        d_list = [cp.adjoint() for cp in c_list]
+        return c_list, d_list
+    
+    def build_hamiltonian(self) -> SparsePauliOp:
+        """
+        Map one- and two-electron integrals to quantum operators
+        args:
+            ecore (float): Core Hamiltonian
+            h1e (numpy ndarray): one-electron integrals
+            h2e (numpy ndarray): two-electron integrals
+        
+        returns:
+            Hamiltonian in fermionic operators
+        """
+
+        # Get number of orbitals
+        ncas, _ = self.h1e.shape
+        # List of fermionic creation and annihilation operators
+        C, D = self.qubit_mapping(2 * ncas, mapping="jordan_wigner")
+        Exc = []
+        for p in range(ncas):
+            Excp = [C[p] @ D[p] + C[ncas + p] @ D[ncas + p]]
+            for r in range(p + 1, ncas):
+                Excp.append(
+                    C[p] @ D[r]
+                    + C[ncas + p] @ D[ncas + r]
+                    + C[r] @ D[p]
+                    + C[ncas + r] @ D[ncas + p]
+                )
+            Exc.append(Excp)
+ 
+        # Low-rank decomposition of the Hamiltonian
+        Lop, ng = self.cholesky(1e-6)
+        t1e = self.h1e - 0.5 * np.einsum("pxxr->pr", self.h2e)
+
+        # Core term
+        H = self.ecore * self.identity(2 * ncas)
+
+        # One-body term
+        for p in range(ncas):
+            for r in range(p, ncas):
+                H += t1e[p, r] * Exc[p][r - p]
+
+        # Two-body term
+        for g in range(ng):
+            Lg = 0 * self.identity(2 * ncas)
+            for p in range(ncas):
+                for r in range(p, ncas):
+                    Lg += Lop[p, r, g] * Exc[p][r - p]
+            H += 0.5 * Lg @ Lg
+
+        return H.chop().simplify()
+
+    def minimize_energy(self, maxiter):
+
+        # Build Hamiltonian and generate operator pool
+        
+        # Data for adapt-VQE iterations
         iteration = 1
-        appended_ops = [SparsePauliOp(["I" * nqubits])]
-        paramstring = [0.0]
-
         e_last = 0
-
         thr = 1e-6
 
-        while iteration <= max_iter:
+        
+        while iteration <= maxiter:
 
+            # List for commutators
             comm_lst = []
 
-            for operator in operator_pool:
-                comm_lst.append(commutator(q_hamiltonian, operator, appended_ops, paramstring))
+            # Compute commutators between the Hamiltonain and operators in pool
+            for operator in self.operator_pool:
+                comm_lst.append(self.commutator(self.hamiltonian, operator))
 
-            # Find largest commutator and apply
-            appended_ops.append(operator_pool[comm_lst.index(min(comm_lst))])
-            paramstring.append(0.0)
+            # Find commutator with largest energy decrease and apply
+            self.appended_ops.append(self.operator_pool[comm_lst.index(min(comm_lst))])
+            self.paramstring.append(0.0)
 
-            param_norm_last = np.sqrt(sum([theta ** 2 for theta in paramstring]))
-            paramstring = list(optimize_params(paramstring))
-            param_norm = np.sqrt(sum([theta ** 2 for theta in paramstring]))
-
-            print("Change in norm is:", param_norm_last - param_norm)
-            if abs(param_norm_last - param_norm) < thr:
-                print("Final energy:", e)
-                break
+            # Calculate norm of parameter vector
+            #param_norm_last = np.sqrt(sum([theta ** 2 for theta in self.paramstring]))
+            #self.paramstring = list(optimize_params(self.paramstring))
+            #param_norm = np.sqrt(sum([theta ** 2 for theta in self.paramstring]))
 
             # Measure energy
-            e = energy(paramstring)[0]
-            print("Appended operator is:", appended_ops[-1])
+            e = self.energy(self.paramstring)
+            print("Appended operator is:", self.appended_ops[-1])
             print(f"Energy at iteration {iteration} is:", e)
             print("\n")
 
@@ -130,4 +293,4 @@ class QubitAdaptVQE():
 
             e_last = e
 
-        iteration += 1
+            iteration += 1
--- project/qiskit-iqm/venv/noisy-tests/mol_adapt_vqe.py
+++ project/qiskit-iqm/venv/noisy-tests/mol_adapt_vqe.py
@@ -1,265 +1,61 @@
-from iqm.qiskit_iqm import IQMProvider
+from adapt_vqe import QubitAdaptVQE
 from iqm.qiskit_iqm.fake_backends import IQMFakeAdonis
-
-from qiskit import QuantumCircuit, QuantumRegister, transpile
-from qiskit.circuit.library import PauliEvolutionGate
-
-from qiskit.primitives import Estimator
-from qiskit.quantum_info import SparsePauliOp
 
 import numpy as np
 import cmath
 
-from scipy.optimize import minimize
-
-from pyscf import ao2mo, gto, mcscf, scf
-
-def cholesky(V, eps):
-    # see https://arxiv.org/pdf/1711.02242.pdf section B2
-    # see https://arxiv.org/abs/1808.02625
-    # see https://arxiv.org/abs/2104.08957
-    no = V.shape[0]
-    chmax, ng = 20 * no, 0
-    W = V.reshape(no**2, no**2)
-    L = np.zeros((no**2, chmax))
-    Dmax = np.diagonal(W).copy()
-    nu_max = np.argmax(Dmax)
-    vmax = Dmax[nu_max]
-    while vmax > eps:
-        L[:, ng] = W[:, nu_max]
-        if ng > 0:
-            L[:, ng] -= np.dot(L[:, 0:ng], (L.T)[0:ng, nu_max])
-        L[:, ng] /= np.sqrt(vmax)
-        Dmax[: no**2] -= L[: no**2, ng] ** 2
-        ng += 1
-        nu_max = np.argmax(Dmax)
-        vmax = Dmax[nu_max]
-    L = L[:, :ng].reshape((no, no, ng))
-    print(
-        "accuracy of Cholesky decomposition ",
-        np.abs(np.einsum("prg,qsg->prqs", L, L) - V).max(),
-    )
-    return L, ng
-
-def identity(n):
-    return SparsePauliOp.from_list([("I" * n, 1)])
-
-def qubit_mapping(n, mapping = "jordan_wigner"):
-    c_list = []
-    if mapping == "jordan_wigner":
-        for p in range(n):
-            if p == 0:
-                ell, r = "I" * (n - 1), ""
-            elif p == n - 1:
-                ell, r = "", "Z" * (n - 1)
-            else:
-                ell, r = "I" * (n - p - 1), "Z" * p
-            cp = SparsePauliOp.from_list([(ell + "X" + r, 0.5), (ell + "Y" + r, 0.5j)])
-            c_list.append(cp)
-    else:
-        raise ValueError("Unsupported mapping.")
-    d_list = [cp.adjoint() for cp in c_list]
-    return c_list, d_list
-
-def build_hamiltonian(ecore: float, h1e: np.ndarray, h2e: np.ndarray) -> SparsePauliOp:
-    ncas, _ = h1e.shape
- 
-    C, D = qubit_mapping(2 * ncas, mapping="jordan_wigner")
-    Exc = []
-    for p in range(ncas):
-        Excp = [C[p] @ D[p] + C[ncas + p] @ D[ncas + p]]
-        for r in range(p + 1, ncas):
-            Excp.append(
-                C[p] @ D[r]
-                + C[ncas + p] @ D[ncas + r]
-                + C[r] @ D[p]
-                + C[ncas + r] @ D[ncas + p]
-            )
-        Exc.append(Excp)
- 
-    # low-rank decomposition of the Hamiltonian
-    Lop, ng = cholesky(h2e, 1e-6)
-    t1e = h1e - 0.5 * np.einsum("pxxr->pr", h2e)
- 
-    H = ecore * identity(2 * ncas)
-    # one-body term
-    for p in range(ncas):
-        for r in range(p, ncas):
-            H += t1e[p, r] * Exc[p][r - p]
-    # two-body term
-    for g in range(ng):
-        Lg = 0 * identity(2 * ncas)
-        for p in range(ncas):
-            for r in range(p, ncas):
-                Lg += Lop[p, r, g] * Exc[p][r - p]
-        H += 0.5 * Lg @ Lg
- 
-    return H.chop().simplify()
-
-def run_qc(qc, shots):
-    backend = IQMFakeAdonis()
-    trans_c = transpile(qc, backend=backend)
-    job = backend.run(trans_c, shots=shots)
-    result = job.result()
-    exp_result = job.result()._get_experiment(qc)
-
-    print(result.get_counts())
-
-def commutator(a, b, appended_ops, paramstring):
-
-    #psi = QuantumCircuit(nqubits)
-    psi = prepare_ansatz(nqubits)
-    paulistring = sum(appended_ops)
-    evolution = PauliEvolutionGate(paulistring, time=1)
-    psi.compose(evolution, inplace=True)
-
-    operator = a @ b - b @ a
-    
-    estimator = Estimator()
-    exp_val = estimator.run(psi, operator).result().values
-
-    exp_val = 1.0j * exp_val
-    print("\nOperator:", b, "Commutator:", exp_val, "\n")
-
-    return exp_val
-
-
-def energy(paramstring):
-
-    #psi = QuantumCircuit(nqubits)
-    psi = prepare_ansatz(nqubits)
-
-    paulistring = SparsePauliOp([op.paulis[0] for op in appended_ops], coeffs = paramstring)
-    evolution = PauliEvolutionGate(paulistring, time=1)
-    psi.compose(evolution, inplace=True)
-
-    estimator = Estimator()
-    e = estimator.run(psi, q_hamiltonian).result().values
-
-    return e
-
-def optimize_params(paramstring):
-
-    theta = paramstring
-    res = minimize(energy, theta, method = "nelder-mead", options={'xatol': 1e-8, 'disp': True})
-
-    return res.x
-
-def prepare_ansatz(nqubits):
-
-    ansatz = QuantumCircuit(nqubits)
-    #ansatz.x(0)
-    #ansatz.x(1)
-    #evolution = PauliEvolutionGate(q_hamiltonian, time=1)
-    #ansatz.compose(evolution, inplace=True)
-
-    return ansatz
+from pyscf import ao2mo, gto, mcscf, scf, lo
+from pyscf.tools import dump_mat
 
 if __name__ == "__main__":
 
-    #a = 0.735 / 2
-    a = 1.5
-    
+    # Create molecule object
+    #a = 1.5
+    a = 2.0
     mol = gto.Mole()
     mol.build(
         verbose = 0,
-        atom = [["He", (0, 0, 0)]],#, ["H", (0, 0, a)]],
-        basis = "sto-3g",
+        atom = [["H", (0, 0, 0)], ["H", (0, 0, a)]],
+        basis = "cc-pVDZ",
         spin = 0,
         charge = 0,
-        #symmetry = "Dooh"
+        symmetry = "Dooh"
     )
 
+    # Run classical SCF to obtain ansatz
     mf = scf.RHF(mol)
     mf.scf()
-    
-    print("HF electronic energy:", mf.energy_elec()[0])
+    print("HF energy:", mf.energy_tot())
 
+    # Orbitals
+    #dump_mat.dump_mo(mol, mf.mo_coeff)
+    
+    # Define active space
+    active_space = range(mol.nelectron // 2 - 1, mol.nelectron // 2 + 1)
+    #active_space = range(4)
+    #active_space = range(10)
+
+
+    # Get 1 and 2 electron Hamiltonians
     #ecore = mf.get_hcore()
     #h1e = mf.get_ovlp()
     #h2e = mol.intor("int2e", aosym="s8")
-    
-    #active_space = range(mol.nelectron // 2 - 1, mol.nelectron // 2 + 1)
-    active_space = range(10)
-
     E1 = mf.kernel()
-    mx = mcscf.CASCI(mf, ncas=10, nelecas=(1, 1))
-    #mo = mx.sort_mo(active_space, base=0)
-    #E2 = mx.kernel(mo)[:2]
-    
+    mx = mcscf.CASCI(mf, ncas=len(active_space), nelecas=(1, 1))
+    mo = mx.sort_mo(active_space, base=0)
+    E2 = mx.kernel(mo)[:2]
     h1e, ecore = mx.get_h1eff()
-    #h2e = ao2mo.restore(1, mx.get_h2eff(), mx.ncas)
-    h2e = mx.get_h2eff()
+    h2e = ao2mo.restore(1, mx.get_h2eff(), mx.ncas)
+    #h2e = mx.get_h2eff()
 
-    print(ecore)
-    print(h1e)
-    print(h2e)
+    # Create qubit-adapt-VQE object
+    vqe = QubitAdaptVQE(ecore, h1e, h2e, "cobyla")
 
+    # Data for qubit-adapt-VQE
+    vqe.set_backend(IQMFakeAdonis())
 
-    #operator_pool = [SparsePauliOp(["IIZY"]), SparsePauliOp(["IIYI"]), SparsePauliOp(["IZYI"]), SparsePauliOp(["IYII"]), SparsePauliOp(["ZYII"]), SparsePauliOp(["YIII"])]
-    operator_pool = [SparsePauliOp(["ZY"]), SparsePauliOp(["YI"])]
-    q_hamiltonian = build_hamiltonian(ecore, h1e, h2e)
+    # Prepare initial state and run
+    print("Initial energy:", vqe.energy([0.0]))
 
-    print(q_hamiltonian)
-    
-    nqubits = 2
-    shots = 1000
-    max_iter = 100
-
-    iteration = 1
-    appended_ops = [identity(nqubits)]
-
-    paramstring = [0.0]
-
-    e_last = 0
-
-    thr = 1e-6
-
-    psi = prepare_ansatz(nqubits)
-    estimator = Estimator()
-    e = estimator.run(psi, q_hamiltonian).result().values
-    print("Initial energy:", e)
-
-    while iteration <= max_iter:
-
-        comm_lst = []
-
-        for operator in operator_pool:
-            comm_lst.append(commutator(q_hamiltonian, operator, appended_ops, paramstring))
-
-        # Find largest commutator and apply
-        appended_ops.append(operator_pool[comm_lst.index(min(comm_lst))])
-        paramstring.append(0.0)
-
-        param_norm_last = np.sqrt(sum([theta ** 2 for theta in paramstring]))
-        paramstring = list(optimize_params(paramstring))
-        param_norm = np.sqrt(sum([theta ** 2 for theta in paramstring]))
-
-        e = energy(paramstring)[0]
-
-        print("Change in norm is:", param_norm_last - param_norm)
-        if abs(param_norm_last - param_norm) < thr:
-            print("Initial HF energy:", mf.energy_tot())
-            print("Final energy:", e)
-            break
-
-
-        # Measure energy
-        print("Appended operator is:", appended_ops[-1], "with coefficient: ", paramstring[-1])
-        print(f"Energy at iteration {iteration} is:", e)
-        print("\n")
-
-        #comm_norm = np.sqrt(sum([comm ** 2 for comm in comm_lst]))
-        
-        # Converged?
-        if iteration > 1:
-            e_diff = e - e_last
-            print("Ediff =", e_diff)
-            if abs(e_diff) < thr:
-                print("Initial HF energy:", mf.energy_tot())
-                print("Final energy:", e)
-                break
-            
-        e_last = e
-
-        iteration += 1
+    # adapt-VQE iterations
+    vqe.minimize_energy(maxiter = 100)
